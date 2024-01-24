82d4c892bc2e3590cb3d980d0681f99e42b83382
Anna <anniegabriento51@gmail.com>
2024-01-24 18:42:14 +0800

    remove routing folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-
-import WebRoutes from "./routing/WebRoutes";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login/login";
+import Signup from "./pages/signup/signup";
+import Dashboard from "./pages/dashboard/dashboard";
 
 function App() {
   return (
     <>
-      <div>
-        <WebRoutes />
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
     </>
   );
 }
